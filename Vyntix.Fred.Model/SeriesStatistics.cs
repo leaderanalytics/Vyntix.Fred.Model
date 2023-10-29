249b6228e15030acf3d998e52c6314249ab43816
Sam <sam.wheat@outlook.com>
2023-10-29 10:28:26 -0700

    Add SeriesStatistics

---
+++ Vyntix.Fred.Model/SeriesStatistics.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaderAnalytics.Vyntix.Fred.Model;
+
+public class SeriesStatistics
+{
+    public int ObservationCount { get; set; }
+    public int VintageCount { get; set; }
+    public int NullValueCount { get; set; }
+    public DateTime FirstVintageDate { get; set; }
+    public DateTime LastVintageDate { get; set; }
+    public DateTime FirstObservationDate { get; set; }
+    public DateTime LastObservationDate { get; set;}
+
+}
