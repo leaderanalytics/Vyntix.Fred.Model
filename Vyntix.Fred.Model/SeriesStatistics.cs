d608fc13409e08fde2e79a475fa09c207f5f9d99
Sam <sam.wheat@outlook.com>
2023-11-03 19:49:20 -0700

    Add Min/Max value

--- Vyntix.Fred.Model/SeriesStatistics.cs
+++ Vyntix.Fred.Model/SeriesStatistics.cs
@@ -5,6 +5,8 @@ public class SeriesStatistics
     public int ObservationCount { get; set; }
     public int VintageCount { get; set; }
     public int NullValueCount { get; set; }
+    public decimal? MinValue { get; set; }
+    public decimal? MaxValue { get; set; }
     public DateTime? FirstVintageDate { get; set; }
     public DateTime? LastVintageDate { get; set; }
     public DateTime? FirstObservationDate { get; set; }
