7e3a033b9c9275cc942286657a62e281a6faf90b
Sam <sam.wheat@outlook.com>
2023-11-03 18:46:37 -0700

    Make dates nullable

--- Vyntix.Fred.Model/SeriesStatistics.cs
+++ Vyntix.Fred.Model/SeriesStatistics.cs
@@ -5,9 +5,9 @@ public class SeriesStatistics
     public int ObservationCount { get; set; }
     public int VintageCount { get; set; }
     public int NullValueCount { get; set; }
-    public DateTime FirstVintageDate { get; set; }
-    public DateTime LastVintageDate { get; set; }
-    public DateTime FirstObservationDate { get; set; }
-    public DateTime LastObservationDate { get; set;}
+    public DateTime? FirstVintageDate { get; set; }
+    public DateTime? LastVintageDate { get; set; }
+    public DateTime? FirstObservationDate { get; set; }
+    public DateTime? LastObservationDate { get; set;}
 
 }
